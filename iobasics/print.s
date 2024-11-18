90d440940804c8a450f3f292cb7ee4d2423cb193
Khanh Hua <giakhanh2487@gmail.com>
2024-11-18 10:27:55 +0100

    Replace static memory buffer bss with stack reservation

--- iobasics/print.s
+++ iobasics/print.s
@@ -2,8 +2,6 @@ section .data
         num     dd      192
 
 section .bss
-        str:    resb    3
-        strlen: equ     $-str
 
 section .text
 
@@ -16,23 +14,25 @@ global _start
         int 0x80
 
         print_int:
-        lea ecx, [str + strlen - 1]
+        sub esp, 0x03           ; Reserves 3 bytes on stack for char str[3]
+        lea ecx, [esp + 0x02]
         mov eax, [num]
         mov ebx, 0x0A
         .loop:
         mov edx, 0
         div ebx
         add edx, 48     ; ASCII character "0" offset
-        or [ecx], edx
-        sub ecx, 1
+        or [ecx], edx   ; Write edx to the head of str
+        sub ecx, 1      ; "Append" an empty head to str, in final interation it points to invalid memory location
         test eax, 0xFFFF
         jnz .loop
 
-        mov ecx, str
+        lea ecx, [esp]
         mov eax, 0x04
         mov ebx, 0x01
-        mov edx, strlen
+        mov edx, 3
         int 0x80
 
+        add esp, 0x03
         ret
 
