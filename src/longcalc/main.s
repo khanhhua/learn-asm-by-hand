b41a3473f035387537fff46216cff7aefc56c9a1
Khanh Hua <giakhanh2487@gmail.com>
2025-10-20 17:54:41 +0200

    Long calculation

--- src/longcalc/main.s
+++ src/longcalc/main.s
@@ -1,3 +1,8 @@
+%macro FREE_POP 0
+  mov dword [esp], 0x00
+  add esp, 4
+%endmacro
+
 %macro PRINT 2
   mov eax, 0x04
   mov ecx, %1
@@ -11,6 +16,10 @@
   lea ecx, %1
   mov edx, 80
   int 0x80
+
+  dec eax
+  add ecx, eax
+  mov byte [ecx], 0x00
 %endmacro
 
 %macro EXIT 0
@@ -18,6 +27,52 @@
   mov ebx, 0
   int 0x80
 %endmacro
+
+%macro CTOI 1
+  sub %1, 48
+%endmacro
+
+atoi:                   ; atoi(out int*, in char* string, in int length), flag eax
+  mov eax, 0
+  mov ecx, [esp + 4]    ; load i into forloop
+  mov ebx, [esp + 8]    ; points ebx to the input buffer
+
+LOOPY:
+  push ebx              ; save current buffer pointer
+  movzx ebx, byte [ebx]
+  CTOI ebx
+
+  push ecx
+  mov ecx, 10
+  mul ecx               ; shift left base-10
+  add eax, ebx          ; .. add the decoded ebx to eax
+  pop ecx
+
+  dec ecx
+  jecxz LOOPY_END
+  
+  pop ebx               ; load current buffer pointer
+  inc ebx
+  cmp byte [ebx], 0
+  jne LOOPY
+
+LOOPY_END:
+  mov ebx, [esp + 12]
+  mov dword [ebx], eax
+  xor eax, eax
+
+  ret
+
+strlen:                 ; void strlen(out int*, in char*)
+  mov eax, 0x00            ; find the NULL character
+  mov edi, [esp + 4]
+  mov ecx, 80
+  repne scasb
+
+  mov dword [esp + 8], 80
+  sub dword [esp + 8], ecx
+  
+  ret
 
 section .data
 S1:       db "Enter operator:",0
@@ -31,6 +86,8 @@ S3_LEN:   equ $-S3
 
 section .bss
 INPUT:    resb 80
+; Allocating 3 dwords on the stack: a, b, and c
+OPERAND_A resb 4
 
 section .text
   global _start
@@ -43,5 +100,20 @@ _start:
   PRINT S1, S1_LEN
   READLINE INPUT
   PRINT INPUT, 80
+
+  ; atoi(
+  push OPERAND_A
+  push INPUT
+  ; strlen(
+  push 0
+  push INPUT
+  call strlen
+  FREE_POP
+  ; )
+
+  call atoi
+  FREE_POP
+  FREE_POP
+  ; )
 
   EXIT
